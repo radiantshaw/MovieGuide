e4d68cb17cac7e51b2c7f835fd89b9eed7b6e5a6
Nipun <nipunparadkar123@gmail.com>
2018-06-23 17:58:04 +0530

    Made MainActivity the parent. Added Preferences. Mapped JSON variables to Object variables.

--- app/src/main/java/com/semicolon/movieguide/MainActivity.java
+++ app/src/main/java/com/semicolon/movieguide/MainActivity.java
@@ -1,14 +1,39 @@
 package com.semicolon.movieguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Movie> data;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.choice_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +51,25 @@ public class MainActivity extends AppCompatActivity {
     private void setupExampleData() {
         data = new ArrayList<>();
 
-        data.add(new Movie("One", "https://dummyimage.com/400x600/000/fff&text=1", "https://dummyimage.com/600x400/000/fff&text=backdrop+1", "First movie", 2.5F, "1/1/1"));
-        data.add(new Movie("Two", "https://dummyimage.com/400x600/000/fff&text=2", "https://dummyimage.com/600x400/000/fff&text=backdrop+2", "Second movie", 4.3F, "2/2/2"));
-        data.add(new Movie("Three", "https://dummyimage.com/400x600/000/fff&text=3", "https://dummyimage.com/600x400/000/fff&text=backdrop+3", "Third movie", 1.2F, "3/3/3"));
-        data.add(new Movie("Four", "https://dummyimage.com/400x600/000/fff&text=4", "https://dummyimage.com/600x400/000/fff&text=backdrop+4", "Fourth movie", 3.6F, "4/4/4"));
-        data.add(new Movie("One", "https://dummyimage.com/400x600/000/fff&text=1", "https://dummyimage.com/600x400/000/fff&text=backdrop+1", "First movie", 2.5F, "1/1/1"));
-        data.add(new Movie("Two", "https://dummyimage.com/400x600/000/fff&text=2", "https://dummyimage.com/600x400/000/fff&text=backdrop+2", "Second movie", 4.3F, "2/2/2"));
-        data.add(new Movie("Three", "https://dummyimage.com/400x600/000/fff&text=3", "https://dummyimage.com/600x400/000/fff&text=backdrop+3", "Third movie", 1.2F, "3/3/3"));
-        data.add(new Movie("Four", "https://dummyimage.com/400x600/000/fff&text=4", "https://dummyimage.com/600x400/000/fff&text=backdrop+4", "Fourth movie", 3.6F, "4/4/4"));
-        data.add(new Movie("One", "https://dummyimage.com/400x600/000/fff&text=1", "https://dummyimage.com/600x400/000/fff&text=backdrop+1", "First movie", 2.5F, "1/1/1"));
-        data.add(new Movie("Two", "https://dummyimage.com/400x600/000/fff&text=2", "https://dummyimage.com/600x400/000/fff&text=backdrop+2", "Second movie", 4.3F, "2/2/2"));
-        data.add(new Movie("Three", "https://dummyimage.com/400x600/000/fff&text=3", "https://dummyimage.com/600x400/000/fff&text=backdrop+3", "Third movie", 1.2F, "3/3/3"));
-        data.add(new Movie("Four", "https://dummyimage.com/400x600/000/fff&text=4", "https://dummyimage.com/600x400/000/fff&text=backdrop+4", "Fourth movie", 3.6F, "4/4/4"));
-        data.add(new Movie("One", "https://dummyimage.com/400x600/000/fff&text=1", "https://dummyimage.com/600x400/000/fff&text=backdrop+1", "First movie", 2.5F, "1/1/1"));
-        data.add(new Movie("Two", "https://dummyimage.com/400x600/000/fff&text=2", "https://dummyimage.com/600x400/000/fff&text=backdrop+2", "Second movie", 4.3F, "2/2/2"));
-        data.add(new Movie("Three", "https://dummyimage.com/400x600/000/fff&text=3", "https://dummyimage.com/600x400/000/fff&text=backdrop+3", "Third movie", 1.2F, "3/3/3"));
-        data.add(new Movie("Four", "https://dummyimage.com/400x600/000/fff&text=4", "https://dummyimage.com/600x400/000/fff&text=backdrop+4", "Fourth movie", 3.6F, "4/4/4"));
-        data.add(new Movie("One", "https://dummyimage.com/400x600/000/fff&text=1", "https://dummyimage.com/600x400/000/fff&text=backdrop+1", "First movie", 2.5F, "1/1/1"));
-        data.add(new Movie("Two", "https://dummyimage.com/400x600/000/fff&text=2", "https://dummyimage.com/600x400/000/fff&text=backdrop+2", "Second movie", 4.3F, "2/2/2"));
-        data.add(new Movie("Three", "https://dummyimage.com/400x600/000/fff&text=3", "https://dummyimage.com/600x400/000/fff&text=backdrop+3", "Third movie", 1.2F, "3/3/3"));
-        data.add(new Movie("Four", "https://dummyimage.com/400x600/000/fff&text=4", "https://dummyimage.com/600x400/000/fff&text=backdrop+4", "Fourth movie", 3.6F, "4/4/4"));
+        data.add(new Movie("Jurassic World: Fallen Kingdom", "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg", "http://image.tmdb.org/t/p/w500/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg", "A volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar, where the creatures have freely roamed for several years after the demise of an animal theme park known as Jurassic World. Claire Dearing, the former park manager, has now founded the Dinosaur Protection Group, an organization dedicated to protecting the dinosaurs. To help with her cause, Claire has recruited Owen Grady, a former dinosaur trainer who worked at the park, to prevent the extinction of the dinosaurs once again.", 6.8F,"2018-06-06"));
+        data.add(new Movie("Deadpool 2", "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 7.7F,"2018-05-15"));
+        data.add(new Movie("Incredibles 2", "http://image.tmdb.org/t/p/w185/hL9Uz2vq93vi20oxZEBBaSs4w8U.jpg", "http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg", "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.", 7.6F,"2018-06-14"));
+        data.add(new Movie("Thor: Ragnarok", "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg", "http://image.tmdb.org/t/p/w500/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg", "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.", 7.4F,"2017-10-25"));
+        data.add(new Movie("Jurassic World: Fallen Kingdom", "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg", "http://image.tmdb.org/t/p/w500/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg", "A volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar, where the creatures have freely roamed for several years after the demise of an animal theme park known as Jurassic World. Claire Dearing, the former park manager, has now founded the Dinosaur Protection Group, an organization dedicated to protecting the dinosaurs. To help with her cause, Claire has recruited Owen Grady, a former dinosaur trainer who worked at the park, to prevent the extinction of the dinosaurs once again.", 6.8F,"2018-06-06"));
+        data.add(new Movie("Deadpool 2", "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 7.7F,"2018-05-15"));
+        data.add(new Movie("Incredibles 2", "http://image.tmdb.org/t/p/w185/hL9Uz2vq93vi20oxZEBBaSs4w8U.jpg", "http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg", "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.", 7.6F,"2018-06-14"));
+        data.add(new Movie("Thor: Ragnarok", "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg", "http://image.tmdb.org/t/p/w500/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg", "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.", 7.4F,"2017-10-25"));
+        data.add(new Movie("Jurassic World: Fallen Kingdom", "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg", "http://image.tmdb.org/t/p/w500/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg", "A volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar, where the creatures have freely roamed for several years after the demise of an animal theme park known as Jurassic World. Claire Dearing, the former park manager, has now founded the Dinosaur Protection Group, an organization dedicated to protecting the dinosaurs. To help with her cause, Claire has recruited Owen Grady, a former dinosaur trainer who worked at the park, to prevent the extinction of the dinosaurs once again.", 6.8F,"2018-06-06"));
+        data.add(new Movie("Deadpool 2", "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 7.7F,"2018-05-15"));
+        data.add(new Movie("Incredibles 2", "http://image.tmdb.org/t/p/w185/hL9Uz2vq93vi20oxZEBBaSs4w8U.jpg", "http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg", "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.", 7.6F,"2018-06-14"));
+        data.add(new Movie("Thor: Ragnarok", "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg", "http://image.tmdb.org/t/p/w500/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg", "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.", 7.4F,"2017-10-25"));
+        data.add(new Movie("Jurassic World: Fallen Kingdom", "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg", "http://image.tmdb.org/t/p/w500/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg", "A volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar, where the creatures have freely roamed for several years after the demise of an animal theme park known as Jurassic World. Claire Dearing, the former park manager, has now founded the Dinosaur Protection Group, an organization dedicated to protecting the dinosaurs. To help with her cause, Claire has recruited Owen Grady, a former dinosaur trainer who worked at the park, to prevent the extinction of the dinosaurs once again.", 6.8F,"2018-06-06"));
+        data.add(new Movie("Deadpool 2", "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 7.7F,"2018-05-15"));
+        data.add(new Movie("Incredibles 2", "http://image.tmdb.org/t/p/w185/hL9Uz2vq93vi20oxZEBBaSs4w8U.jpg", "http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg", "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.", 7.6F,"2018-06-14"));
+        data.add(new Movie("Thor: Ragnarok", "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg", "http://image.tmdb.org/t/p/w500/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg", "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.", 7.4F,"2017-10-25"));
+        data.add(new Movie("Jurassic World: Fallen Kingdom", "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg", "http://image.tmdb.org/t/p/w500/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg", "A volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar, where the creatures have freely roamed for several years after the demise of an animal theme park known as Jurassic World. Claire Dearing, the former park manager, has now founded the Dinosaur Protection Group, an organization dedicated to protecting the dinosaurs. To help with her cause, Claire has recruited Owen Grady, a former dinosaur trainer who worked at the park, to prevent the extinction of the dinosaurs once again.", 6.8F,"2018-06-06"));
+        data.add(new Movie("Deadpool 2", "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg", "http://image.tmdb.org/t/p/w500/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg", "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.", 7.7F,"2018-05-15"));
+        data.add(new Movie("Incredibles 2", "http://image.tmdb.org/t/p/w185/hL9Uz2vq93vi20oxZEBBaSs4w8U.jpg", "http://image.tmdb.org/t/p/w500/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg", "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.", 7.6F,"2018-06-14"));
+        data.add(new Movie("Thor: Ragnarok", "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg", "http://image.tmdb.org/t/p/w500/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg", "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.", 7.4F,"2017-10-25"));
     }
 }
--- app/src/main/java/com/semicolon/movieguide/Movie.java
+++ app/src/main/java/com/semicolon/movieguide/Movie.java
@@ -1,11 +1,18 @@
 package com.semicolon.movieguide;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
     private String title;
+    @SerializedName("poster_path")
     private String posterURL;
+    @SerializedName("backdrop_path")
     private String backdropURL;
+    @SerializedName("overview")
     private String synopsis;
+    @SerializedName("vote_average")
     private float rating;
+    @SerializedName("release_date")
     private String releaseDate;
 
     public Movie(String title, String posterURL, String backdropURL, String synopsis, float rating, String releaseDate) {
